2263440d98d4bed92e62cbdfc7d8b443896ec7f8
Tsaku David Yakubu <dave4b10@gmail.com>
2022-10-14 15:18:35 -0700

    print number combo

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,41 +7,27 @@
  */
 int main(void)
 {
-int i, e, g, h, op1, op2;
+	int i, j;
 
-i = e = g = h = 48;
-while (h < 58)
-{
-	g = 48;
-	while (g < 58)
+	for (i = 0; i < 100; i++)
 	{
-		e = 48;
-		while (e < 58)
+		for (j = 0; j < 100; j++)
 		{
-			i = 48;
-			while (i < 58)
+			if (i < j)
 			{
-				op1 = (h * 10) + g;
-				op2 = (e * 10) + i;
-				if (op1 < op2)
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar (' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
 				{
-					putchar(h);
-					putchar(g);
-					putchar(' ');
-					putchar(e);
-					putchar(i);
-					if (h == 57 && g == 56 && e == 57 && i == 57)
-						break;
 					putchar(',');
 					putchar(' ');
 				}
-				i++;
 			}
-			e++;
 		}
-		g++;
 	}
-	h++;
 }
 putchar('\n');
 return (0);
