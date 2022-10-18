95fc72bcc8734ba8661b5874357643e12aba892b
Tsaku David Yakubu <dave4b10@gmail.com>
2022-10-18 07:52:04 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * main - print alphabet in lowercase.
+  *
+  * Return: 0 (Success)
+  */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
