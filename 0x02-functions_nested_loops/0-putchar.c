a236232aaa5a30307156220efad926e792f1f674
Tsaku David Yakubu <dave4b10@gmail.com>
2022-10-18 07:10:48 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,8 +4,6 @@
 *Written by Tsaku David.
 *Return: 0 (Success)
 */
-int main(void)
-{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -16,4 +14,3 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-}
