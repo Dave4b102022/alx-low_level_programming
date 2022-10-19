36bbda3c6cc603cfaa654f62e06606512377545a
Tsaku David Yakubu <dave4b10@gmail.com>
2022-10-18 23:46:15 -0700

    Task 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+  * main - prints the sum of even valued fibonacci sequence
+  * terms not exceeding 4000000.
+  *
+  * Return: Always 0.
+  */
+
+int main(void)
+{
+	unsigned long fib, prevFib, prev2Fib;
+	float sum;
+
+	prevFib = 1;
+	prev2Fib = 0;
+
+	while (1)
+	{
+		fib = prevFib + prev2Fib;
+
+		if (fib > 4000000)
+			break;
+
+		if ((fib % 2) == 0)
+			sum += fib;
+		prev2Fib = prevFib;
+		prevFib = fib;
+	}
+	printf("%.0f\n", sum);
+
+	return (0);
+}
