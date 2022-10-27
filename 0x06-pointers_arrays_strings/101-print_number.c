655548db5523661e20402221aa838d2466afcc90
Tsaku David Yakubu <dave4b10@gmail.com>
2022-10-27 05:47:29 -0700

    Task 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,28 +8,16 @@
 
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	unsigned int x = n;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		m = n * -1;
+		_putchar('-');
+		x = -x;
 	}
-	else
-	{
-		m = n;
-	}
+	if ((x / 10) > 0)
+		print_number(x / 10);
 
-	d = m;
-	count = 1;
+	_putchar(x % 10 + '0');
 
-	while (d > 9)
-	{
-		d /= 10;
-		count *=10;
-	}
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
 }
