28733833a4e14a2011e189feae08b0446cc60de6
Tsaku David Yakubu <dave4b10@gmail.com>
2022-10-27 04:35:38 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,6 +4,7 @@
   * reverse_array - reverses an array of integers
   * @a: array to be reversed
   * @n: number of elements in the array
+  * Return: Nothing
   */
 
 void reverse_array(int *a, int n)
